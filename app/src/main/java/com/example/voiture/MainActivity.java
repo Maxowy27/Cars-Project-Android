a7c16a6ee2f6961f53fbdcd68f230951992b3661
si112114 <si112114@campus.unice.fr>
2023-03-31 16:48:55 +0200

    change editview en searchview

--- app/src/main/java/com/example/voiture/MainActivity.java
+++ app/src/main/java/com/example/voiture/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("Toyota", "Camry", 2020));
+        carList.add(new Car("Honda", "Civic", 2019));
+        carList.add(new Car("Ford", "Mustang", 2021));
+
     }
 }
