b59fec2ae22cb451c96d8a33d83fa26062affaa9
lm103841 <mattys.lemoine@etu.unice.fr>
2023-04-03 23:24:30 +0200

    page connexion

--- app/src/main/java/com/example/voiture/AccountActivity.java
+++ app/src/main/java/com/example/voiture/AccountActivity.java
@@ -1,5 +1,6 @@
 package com.example.voiture;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,11 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        findViewById(R.id.return_button).setOnClickListener(click -> {
+            Intent intent = new Intent(this, AllActivity.class);
+            startActivity(intent);
+        });
 
 
     }
